77ee5c5503753a279de32c457f67c04453f53686
NorrinaDrago <norrinaong@gmail.com>
2023-02-04 17:54:41 +0800

    9

---
+++ 11_Tiffany_Lab09/Assets/Editor/CubeTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class CubeTest
+    {
+        [Test]
+        [TestCase(0)]
+        [TestCase(-26)]
+        public void DisableOnDeath_EmptyHP_ObjectSetInactive(float HP)
+        {
+            GameObject testObject = MakeFakeCube(HP);
+
+            Assert.IsFalse(testObject.activeSelf);
+        }
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(0.6f)]
+        [TestCase(100)]
+        [TestCase(9999)]
+        public void DisableOnDeath_HasHP_ObjectRemainactive(float HP)
+        {
+            GameObject testObject = MakeFakeCube(HP);
+
+            Assert.IsTrue(testObject.activeSelf);
+        }
+
+        private static GameObject MakeFakeCube(float HP)
+        {
+            GameObject testObject = new GameObject();
+            Cube cubeScript = testObject.AddComponent<Cube>();
+
+            cubeScript.Health = HP;
+            cubeScript.DisableOnDeath();
+            return testObject;
+        }
+    }
+}
---
+++ 11_Tiffany_Lab09/Assets/Scripts/Cube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public float Health;
+    // Update is called once per frame
+    void Update()
+    {
+        DisableOnDeath();
+    }
+
+    public void DisableOnDeath()
+    {
+        if (Health <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
